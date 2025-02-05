ce8d9d2c372b6cc413103ab2629419558576ca1c
nferher0909 <nferher0909@g.educaand.es>
2025-02-05 12:56:53 +0100

    Update leer-numeros.js

--- leer-numeros.js
+++ leer-numeros.js
@@ -1,9 +1,9 @@
-document.write ("<br>");
-var numero = prompt("Introduce un número entre 1 y 100:"); 
-numero = Number(numero);
-while (numero < 1 || numero > 100)
-{
-  numero = prompt("Número inválido. Introduce un número entre 1 y 100:");
-numero = Number (numero); 
-}
-document.write ("<br>");
+   var numero =  prompt("Introduce un número entre 1 y 10000:"); 
+            numero = Number(numero); 
+            
+    while (numero < 1 || numero > 10000)
+                        {
+            numero = prompt("Número inválido. Introduce un número entre 1 y 100:");
+            numero = Number (numero); // Convertimos el texto a numero
+                        }
+            document.write ("<br>");
