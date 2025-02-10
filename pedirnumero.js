45d7d94481d42153ee9b970a68a1891fefcb5562
nferher0909 <nferher0909@g.educaand.es>
2025-02-10 12:23:59 +0100

    Update pedirnumero.js

--- pedirnumero.js
+++ pedirnumero.js
@@ -1,9 +1,9 @@
-function pedirNumeroEnRango(1, 50) {
+function pedirnumeroEnRango(1, 10) {
   var numero;
   do {
-    numero = prompt("Ingrese un número entre " + 1 +" y " + 50 + ":");
+    numero = prompt("Ingrese un número entre " + 1 +" y " + 10 + ":");
     numero = Number(numero); 
-  } while (numero < 1 || numero > 50);
+  } while (numero < 1 || numero > 10);
 
   return numero;
 }
