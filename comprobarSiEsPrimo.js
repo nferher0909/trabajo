53f179afd90b002bd72f36c4dd317a5e73ab75e2
nferher0909 <nferher0909@g.educaand.es>
2025-02-12 13:17:51 +0100

    Create comprobarSiEsPrimo.js

---
+++ comprobarSiEsPrimo.js
@@ -0,0 +1,13 @@
+    function comprobarEsPrimo(numero) {
+                var esPrimo;
+                var i;
+              
+                for (i = numero-1; i >= 2; i = i-1) {
+                    if (numero % i === 0) {
+                            esPrimo = false;
+                            return esPrimo;
+                            }
+                 }
+                 esPrimo = true;
+                 return esPrimo;
+            }
